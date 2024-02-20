14645e8985d95131609e7c7bf00aecca0ffb4020
Jen <jenvicen@ttu.edu>
2024-02-19 23:09:20 -0600

    regex complete

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -124,38 +124,34 @@ def test_load_from_regex_():
     assert automaton.process_string("34") == ("REJECT", "String rejected")
     assert automaton.process_string("3466666") == ("ACCEPT", "String accepted")
 
-    '''
     regex = "^J0129"
     automaton = load_from_regex(regex)
     assert isinstance(automaton, DFA)
     assert automaton.start_state == "q0"
-    assert automaton.accept_states == {"q3"}
+    assert automaton.accept_states == {"q5"}
     assert automaton.process_string("J0129") == ("ACCEPT", "String accepted")
-    assert automaton.process_string("9J120") == ("REJECT", "String rejected")
-    assert automaton.process_string("J1209") == ("REJECT", "String rejected")
-    '''
+    assert automaton.process_string("9J120") == ("REJECT", "No transition for '9' in state 'q0'")
+    assert automaton.process_string("J1209") == ("REJECT", "No transition for '1' in state 'q1'")
 
-    '''
     regex = "FAE4$"
     automaton = load_from_regex(regex)
     assert isinstance(automaton, DFA)
     assert automaton.start_state == "q0"
-    assert automaton.accept_states == {"q3"}
-    assert automaton.process_string("J0129") == ("ACCEPT", "String accepted")
-    assert automaton.process_string("9J120") == ("REJECT", "String rejected")
-    assert automaton.process_string("J1209") == ("REJECT", "String rejected")
-    '''
+    assert automaton.accept_states == {"q4"}
+    assert automaton.process_string("FAE4") == ("ACCEPT", "String accepted")
+    assert automaton.process_string("FAE") == ("REJECT", "String rejected")
+    assert automaton.process_string("FAEFAE4") == ("REJECT", "No transition for 'F' in state 'q3'")
 
-    '''
+
     regex = "0*(1234)*ABCD"
     automaton = load_from_regex(regex)
     assert isinstance(automaton, DFA)
     assert automaton.start_state == "q0"
-    assert automaton.accept_states == {"q3"}
-    assert automaton.process_string("J0129") == ("ACCEPT", "String accepted")
-    assert automaton.process_string("9J120") == ("REJECT", "String rejected")
-    assert automaton.process_string("J1209") == ("REJECT", "String rejected")
-    '''
+    assert automaton.accept_states == {"q8"}
+    assert automaton.process_string("000012341234ABCD") == ("ACCEPT", "String accepted")
+    assert automaton.process_string("1234ABCD") == ("ACCEPT", "String accepted")
+    assert automaton.process_string("ABCD") == ("ACCEPT", "String accepted")
+
 
 if __name__ == "__main__":
     pytest.main() 
